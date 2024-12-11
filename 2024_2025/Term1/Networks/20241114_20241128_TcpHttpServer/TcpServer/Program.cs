d92870aa83802d5979e2f38e42d4c414f1587b43
SU-Nesterenko <55326271+SU-Nesterenko@users.noreply.github.com>
2024-12-11 19:04:48 +0500

    2024.12.11 added HttpServerOnTcpClient

---
+++ 2024_2025/Term1/Networks/20241114_20241128_TcpHttpServer/TcpServer/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace TcpServer
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> requestHeaders = new Dictionary<string, string>();
+            Dictionary<string,string> postParams = new Dictionary<string, string>();
+            Dictionary<string, string> getParams = new Dictionary<string, string>();
+
+            string url = "";
+
+            byte[] RequestBuffer = new byte[1024];
+            byte[] ResponceBuffer = new byte[1024];
+
+            string responce_headers, responce_body, responce, _start_line="";
+            string request, _body, __get_params;
+            string[] _headers_body, _headers, _key_val, _post_params, _get_params;
+
+            bool first_line = true;
+
+            
+
+            //Запуск сервера
+            TcpListener server = new TcpListener(IPAddress.Any, 8787);
+            server.Start();
+
+            for (; ; )
+            {
+                first_line = true;
+
+                //Ожидание подключения клиентов
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                Console.WriteLine("Ожидание подключения...");
+
+                Console.ResetColor();
+
+                //Ожидание подключения клиентов
+                TcpClient client = server.AcceptTcpClient();
+
+                Console.WriteLine("Клиент подключен");
+
+                requestHeaders.Clear();
+
+                //Принятие запроса от клиента
+                client.GetStream().Read(RequestBuffer, 0, RequestBuffer.Length);
+                request = Encoding.ASCII.GetString(RequestBuffer);
+
+                //Console.WriteLine(request);
+                TUI.VisualizeText("Содержимое запроса",request);
+
+                if (request.Trim().Trim('\0') == "") {
+                    client.Close();
+                    continue; 
+                }
+
+                //Console.ReadLine();
+
+                //Парсинг заголовков запроса, и их помещение в массив
+                _headers_body= request.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);                
+                _headers = _headers_body[0].Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                foreach(string _header in _headers)
+                {
+                    //Пропуск стартовой строки
+                    if (first_line) { 
+                        first_line = false;
+                        _start_line = _header;
+                        continue; 
+                    }
+                    
+                    if (_header.Trim() == "") continue;
+
+                    //Разбивка заголовка по символу ":" на имя заголовка и значение заголовка
+                    _key_val = _header.Split(new string[] { ":" }, StringSplitOptions.None);
+
+                    //if (_key_val[0].Trim() == "" || _key_val[1].Trim() == "") continue;
+
+                    //Добавление извлечённого заголовка в ассоциативный массив
+                    requestHeaders.Add(_key_val[0],_key_val[1]);
+                }
+
+                //Парсинг полей формы (POST-параметров)
+                if(_headers_body[1].Trim().Trim('\0')!="")
+                { //если тело запроса не пустое
+                    _body = _headers_body[1];
+                    _post_params = _body.Split(new string[] { "&" }, StringSplitOptions.None);
+                    foreach(string _post_param in _post_params)
+                    {
+                        _key_val = _post_param.Split(new string[] { "=" }, StringSplitOptions.None);
+                        postParams.Add(_key_val[0], _key_val[1]);
+                    }
+                }
+
+                //Парсинг GET-параметров и адресной строки
+                Match ReqMatch = Regex.Match(_start_line, @"^\w+\s+([^\s\?]+)\??([^\s]*)\s+HTTP/.*|");
+                url = ReqMatch.Groups[1].Value;
+                if (ReqMatch.Groups.Count >= 2 && ReqMatch.Groups[2].Value.Trim()!="") {
+                    __get_params = ReqMatch.Groups[2].Value;
+                    _get_params = __get_params.Split(new string[] { "&" }, StringSplitOptions.None);
+                    foreach (string _get_param in _get_params)
+                    {
+                        _key_val = _get_param.Split(new string[] { "=" }, StringSplitOptions.None);
+                        getParams.Add(_key_val[0], _key_val[1]);
+                    }
+                }
+
+                TUI.VisualizeArray("Результат парсинга заголовков", requestHeaders);
+                TUI.VisualizeArray("Результат парсинга POST-параметров", postParams);
+                TUI.VisualizeArray("Результат парсинга GET-параметров", getParams);
+                TUI.VisualizeText("Запрошен адрес",url);
+
+                //Формирование ответа клиенту
+                responce_body = "<h1>Hello from Server!</h1>";
+
+                responce_headers = "HTTP/1.1 200 OK\n" +
+                                   "Content-type: text/html\n" +
+                                   "Content-Length: " + responce_body.Length + "\n\n";
+
+                responce = responce_headers + responce_body;
+
+                ResponceBuffer = Encoding.ASCII.GetBytes(responce);
+
+                //Отправка ответа клиенту
+                client.GetStream().Write(ResponceBuffer, 0, ResponceBuffer.Length);
+
+                Thread.Sleep(500);
+
+                //Отсоединение клиента
+                client.Close();
+
+                Console.WriteLine("Клиент отсоединён");
+                Console.Write(new string('-',Console.BufferWidth));
+            }
+
+            server.Stop();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ 2024_2025/Term1/Networks/20241114_20241128_TcpHttpServer/TcpServer/TUI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    class TUI
+    {
+        public static void PadWrite(string text)
+        {
+            Console.WriteLine(text.PadRight(Console.BufferWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+
+        public static void SetColor(ConsoleColor bg, ConsoleColor fg)
+        {
+            Console.BackgroundColor = bg;
+            Console.ForegroundColor = fg;
+        }
+
+        public static void VisualizeText(string title, string text)
+        {
+            SetColor(ConsoleColor.Blue, ConsoleColor.White);
+            PadWrite(title);
+            SetColor(ConsoleColor.Black, ConsoleColor.Green);            
+            PadWrite(text);
+            SetColor(ConsoleColor.Black, ConsoleColor.White);            
+        }
+        public static void VisualizeArray(string title, Dictionary<string, string> array)
+        {
+            int buffer_width= Console.BufferWidth;
+            int title_width = 26;
+            int value_width = buffer_width - title_width;
+
+            
+
+            SetColor(ConsoleColor.Red, ConsoleColor.White);
+            PadWrite(title);  
+
+            foreach (KeyValuePair<string, string> item in array)
+            {
+                SetColor(ConsoleColor.Gray, ConsoleColor.Black);
+                Console.Write("{0,-"+ title_width + "}", item.Key);                
+
+                SetColor(ConsoleColor.White, ConsoleColor.Blue);
+                Console.WriteLine("{0,-"+ value_width + "}",item.Value);
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+            }
+
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
